efbf13a52df1eb33acde1f50df0c74704c226cc4
Aul16 <paul.ferino@student-cs.fr>
2024-06-05 16:08:13 +0200

    Filtres

--- frontend/src/components/FilterSearch/FilterSearch.jsx
+++ frontend/src/components/FilterSearch/FilterSearch.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_KEY } from '../../pages/Home/Home';
+import './FilterSearch.css';
+import { SearchResults } from '../../SearchResults/SearchResults';
 
 const sortChoices = [
   { value: 'original_title', label: 'Titre original' },
@@ -10,17 +14,57 @@ const sortChoices = [
   { value: 'vote_count', label: 'Nombre de votes' },
 ];
 
-export const FilterSearch = () => {
-  const [sortBy, setSortBy] = useState('popularity');
+const useFetchCategories = (currentCategories, sortBy, setMovies) => {
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    console.log('Tri par :', sortBy);
-  }, [sortBy]);
+    fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', {
+      headers: { Authorization: `Bearer ${API_KEY}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data.genres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const category_filter = currentCategories.join('%2C'); // %2C is the URL encoding for a comma
+    let url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&page=1&sort_by=${sortBy}.desc`;
+    if (currentCategories.length !== 0) {
+      console.log(category_filter);
+      url += `&with_genres=${category_filter}`;
+    }
+
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [currentCategories, sortBy, setMovies]);
+
+  return categories;
+};
+
+export const FilterSearch = () => {
+  const [sortBy, setSortBy] = useState('popularity');
+  const [currentCategories, setCurrentCategories] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const categories = useFetchCategories(currentCategories, sortBy, setMovies);
 
   return (
-    <div>
-      <h2>Filtres</h2>
-      <div>
+    <div className="global">
+      <div className="filters">
+        <h2>Filtres</h2>
         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
           {sortChoices.map((choice) => (
             <option key={choice.value} value={choice.value}>
@@ -28,6 +72,31 @@ export const FilterSearch = () => {
             </option>
           ))}
         </select>
+        <h3>Catégories</h3>
+        <ul>
+          {categories.map((category) => (
+            <li key={category.id}>
+              <input
+                type="checkbox"
+                id={category.id}
+                checked={currentCategories.includes(category.id)}
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setCurrentCategories([...currentCategories, category.id]);
+                  } else {
+                    setCurrentCategories(
+                      currentCategories.filter((id) => id !== category.id)
+                    );
+                  }
+                }}
+              />
+              <label htmlFor={category.id}>{category.name}</label>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="movies">
+        <SearchResults loading={false} movies={movies} />
       </div>
     </div>
   );
